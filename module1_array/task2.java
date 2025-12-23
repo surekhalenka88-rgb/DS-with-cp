ef3fe6b104dbddcab78d8cf8beabf4da34f4836a
surekhalenka88-rgb <surekhalenka88@gmail.com>
2025-12-23 12:05:29 +0530

    Refactor Test class to Main and enhance output
    Updated the class structure and added capacity information.

--- module1_array/task2.java
+++ module1_array/task2.java
@@ -1,17 +1,18 @@
-class Test
-{
-public static void main(String args[])
-{
-int cz=0;
-int n[]=new int[5];
-for(int i=0;i<5;i++)
-{
-n[i]=i+1;
-}
-cz=5;
-for(int i=0;i<cz;i++)
-{
-System.out.println(n[i]);
-}
-}
-}
+class Main {
+    public static void main(String[] args) {
+
+        int[] n = new int[10];
+        int cz = 0;
+
+        for (int i = 0; i < 5; i++) {
+            n[i] = i + 1;
+        }
+
+        cz = 5;
+        
+        System.out.println(" total capacity is "+ n.length);
+        
+        System.out.println(" After initializing 5 values manually ");
+        
+        System.out.println(" current size is "+ cz);
+        
