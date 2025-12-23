a42e7e5e363682363ef6c05942bcc3bf7937d7b5
surekhalenka88-rgb <surekhalenka88@gmail.com>
2025-12-23 10:25:45 +0530

    Add Test class with array initialization and output

---
+++ module1_array/task2.java
@@ -0,0 +1,17 @@
+class Test
+{
+public static void main(String args[])
+{
+int cz=0;
+int n[]=new int[5];
+for(int i=0;i<5;i++)
+{
+n[i]=i+1;
+}
+cz=5;
+for(int i=0;i<cz;i++)
+{
+System.out.println(n[i]);
+}
+}
+}
